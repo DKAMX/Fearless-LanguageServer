541458d3183e5085d51023e66a5e5a0b105d9888
Max <47777521+DKAMX@users.noreply.github.com>
2025-01-31 11:21:21 +1300

    feat: implement type assist with visitor

--- src/main/java/io/github/dkamx/fearless/handler/CompletionHandler.java
+++ src/main/java/io/github/dkamx/fearless/handler/CompletionHandler.java
@@ -1,9 +1,9 @@
 package io.github.dkamx.fearless.handler;
 
 import io.github.dkamx.fearless.FearlessLanguageServer;
+import io.github.dkamx.fearless.visitor.ast.IdTypeVisitor;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.logging.Logger;
 import org.eclipse.lsp4j.CompletionItem;
 import org.eclipse.lsp4j.CompletionItemKind;
@@ -12,7 +12,7 @@ import org.eclipse.lsp4j.Position;
 
 public class CompletionHandler {
 
-  public static final CompletionOptions PROVIDER = new CompletionOptions(false, List.of());
+  public static final CompletionOptions PROVIDER = new CompletionOptions(false, List.of("."));
   private static final Logger LOGGER = Logger.getLogger(CompletionHandler.class.getName());
   private final FearlessLanguageServer server;
 
@@ -46,22 +46,47 @@ public class CompletionHandler {
     return createItemList(list, CompletionItemKind.Class);
   }
 
-  List<CompletionItem> completeMethod(String uri) {
-    var info = WorkspaceCacheStore.getProgramInfo(uri);
-    var list = info.types().keySet().stream().toList().stream()
-        .map(info::getMethods)
-        .flatMap(Set::stream)
-        .toList();
-    return createItemList(list, CompletionItemKind.Method);
+  public List<CompletionItem> completeMethod(String uri, Position position) {
+    var list = new ArrayList<CompletionItem>();
+    var content = WorkspaceCacheStore.getFileContent(uri);
+    var range = GrammarTreeHelper.findMinimalE(content, position);
+    if (range == null) {
+      return list;
+    }
+    content = CompletionLogic.assistType(content, range);
+    var program = server.getCompilationHandler().build(uri, content);
+    if (program == null) {
+      return list;
+    }
+    var decs = CompletionLogic.findDecs(program, uri);
+    var visitor = new IdTypeVisitor();
+    decs.forEach(dec -> dec.lambda().accept(visitor));
+    var idTypes = visitor.getResult();
+    idTypes.values().stream().findFirst().ifPresent(
+        type -> {
+          WorkspaceCacheStore.getProgramInfo(uri).getMethods(type).forEach(
+              method -> {
+                var item = new CompletionItem(method);
+                item.setKind(CompletionItemKind.Method);
+                list.add(item);
+              }
+          );
+        }
+    );
+    return list;
   }
 
-  public List<CompletionItem> handle(String uri, Position position) {
+  public List<CompletionItem> handle(String uri, Position position, boolean isTriggerCharacter) {
     LOGGER.info("handle\n%s %s".formatted(uri, position));
     server.infoMessage("handle\n%s %s".formatted(uri, position));
     var list = new ArrayList<CompletionItem>(completeAliasKeyword());
     list.addAll(completeKeyword());
     if (!WorkspaceCacheStore.hasProgram(uri)) {
       server.getCompilationHandler().build(uri);
+    }
+    if (isTriggerCharacter) {
+      // type assist for variable
+      return completeMethod(uri, position);
     }
     list.addAll(completeType(uri));
     return list;
--- src/main/java/io/github/dkamx/fearless/handler/CompletionLogic.java
+++ src/main/java/io/github/dkamx/fearless/handler/CompletionLogic.java
@@ -2,13 +2,49 @@ package io.github.dkamx.fearless.handler;
 
 import ast.E.Meth;
 import ast.Program;
+import ast.T.Dec;
+import files.HasPos;
+import java.net.URI;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.eclipse.lsp4j.Range;
 
 public class CompletionLogic {
+
+  public static String assistType(String content, Range range) {
+    var start = range.getStart();
+    var end = range.getEnd();
+    var lines = content.split("\n");
+
+    var before = new StringBuilder();
+    for (int i = 0; i < start.getLine(); i++) {
+      before.append(lines[i]).append("\n");
+    }
+    before.append(lines[start.getLine()], 0, start.getCharacter());
+
+    var target = new StringBuilder();
+    for (int i = start.getLine(); i <= end.getLine(); i++) {
+      var line = lines[i];
+      target.append(line,
+          i == start.getLine() ? start.getCharacter() : 0,
+          i == end.getLine() ? end.getCharacter() : line.length());
+      if (i != end.getLine()) {
+        target.append("\n");
+      }
+    }
+
+    var after = new StringBuilder();
+    after.append(lines[end.getLine()].substring(end.getCharacter())).append("\n");
+    for (int i = end.getLine() + 1; i < lines.length; i++) {
+      after.append(lines[i]).append("\n");
+    }
+
+    return before + "base.Id.id(" + target + ")" + after;
+  }
 
   public static ProgramInfo collectProgramInfo(Program program) {
     var types = new HashMap<String, Set<String>>();
@@ -26,5 +62,21 @@ public class CompletionLogic {
       methods.put(name, list);
     });
     return new ProgramInfo(types, methods);
+  }
+
+  public static List<Dec> findDecs(Program program, String uri) {
+    return program.ds().values().stream()
+        .filter(dec -> isCurrentFile(dec, uri))
+        .toList();
+  }
+
+  public static boolean isCurrentFile(HasPos dec, String uri) {
+    if (dec.pos().isEmpty()) {
+      return false;
+    }
+    var inputPath = Path.of(URI.create(uri));
+    var decUri = dec.pos().get().fileName();
+    var decPath = Path.of(decUri).toAbsolutePath();
+    return decPath.equals(inputPath);
   }
 }
