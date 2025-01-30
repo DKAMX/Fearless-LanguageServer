ad9e441034f6dc36f07f8063ea257110c072de84
Max <47777521+DKAMX@users.noreply.github.com>
2025-01-31 11:18:30 +1300

    feat: update GrammarTreeHelper for locating minimal E

--- src/main/java/io/github/dkamx/fearless/handler/GrammarTreeHelper.java
+++ src/main/java/io/github/dkamx/fearless/handler/GrammarTreeHelper.java
@@ -3,6 +3,7 @@ package io.github.dkamx.fearless.handler;
 import generated.FearlessLexer;
 import generated.FearlessParser;
 import io.github.dkamx.fearless.visitor.grammar.MinimalEVisitor;
+import io.github.dkamx.fearless.visitor.grammar.MinimalEWithinVisitor;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -16,9 +17,12 @@ public class GrammarTreeHelper {
     return new FearlessParser(new CommonTokenStream(lexer));
   }
 
-  public static Range findMinimalE(FearlessParser parser, Position position) {
-    var visitor = new MinimalEVisitor(position);
-    return visitor.visit(parser.nudeProgram());
+  public static Range findMinimalE(String content, Position position) {
+    return new MinimalEVisitor(position).visit(createParser(content).nudeProgram());
+  }
+
+  public static Range findMinimalEWithin(String content, Position position) {
+    return new MinimalEWithinVisitor(position).visit(createParser(content).nudeProgram());
   }
 
   public static boolean isTouchingTail(ParserRuleContext context, Position position) {
@@ -35,5 +39,29 @@ public class GrammarTreeHelper {
     return start.getLine() - 1 <= line && line <= end.getLine() - 1
         && start.getCharPositionInLine() < character
         && character < end.getCharPositionInLine() + end.getText().length();
+  }
+
+  public static boolean isWithin(Range range, Position position) {
+    var start = range.getStart();
+    var end = range.getEnd();
+    var line = position.getLine();
+    var character = position.getCharacter();
+    return start.getLine() <= line && line <= end.getLine()
+        && start.getCharacter() < character // not touching head/tail
+        && character < end.getCharacter();
+  }
+
+  /**
+   * Check if range A is a proper sub-range of B.
+   */
+  public static boolean isProperSubrange(Range a, Range b) {
+    var startA = a.getStart();
+    var endA = a.getEnd();
+    var startB = b.getStart();
+    var endB = b.getEnd();
+    return (startB.getLine() < startA.getLine()
+        || (startB.getLine() == startA.getLine() && startB.getCharacter() < startA.getCharacter()))
+        && (endB.getLine() > endA.getLine()
+        || (endB.getLine() == endA.getLine() && endB.getCharacter() > endA.getCharacter()));
   }
 }
